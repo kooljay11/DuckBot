39bdf99aaf9b89b5f53eb21dfa866065b4bc3894
kooljay11 <86942676+kooljay11@users.noreply.github.com>
2023-12-29 17:10:26 -0800

    Implemented quack titles and daily reset

--- app.py
+++ app.py
@@ -13,8 +13,11 @@ client = commands.Bot(command_prefix="/",
                       intents=discord.Intents.all())
 
 
+# @tasks.loop(time=[datetime.time(hour=12, tzinfo=datetime.timezone.utc)])
 @tasks.loop(time=[datetime.time(hour=12, tzinfo=datetime.timezone.utc)])
+# @tasks.loop(minutes=1)
 async def dailyReset():
+    print('Daily reset occurring')
     # with open("./bot_status.txt", "r") as file:
     #     randomresponses = file.readlines()
     #     response = random.choice(randomresponses)
@@ -22,15 +25,33 @@ async def dailyReset():
     with open("./user_info.json", "r") as file:
         user_info = json.load(file)
 
-    for userId, user in user_info["users"].items():
+    # Reset streak counter if the streak is broken
+    for userId, user in user_info.items():
         if not bool(user["quackedToday"]):
             user["quackStreak"] = 0
 
         user["quackedToday"] = False
 
+        target_rank = await get_quack_rank(user["quacks"])
+        print(f'target_rank: {target_rank}')
+
+        if target_rank != user["quackRank"]:
+            user["quackRank"] = target_rank
+
     # Save to database
     with open("./user_info.json", "w") as file:
         json.dump(user_info, file, indent=4)
+
+    # Tell the specified channel about the update
+    try:
+        with open("./global_info.json", "r") as file:
+            global_info = json.load(file)
+
+        destination_channel = int(global_info["new_day_channel_id"])
+        await client.get_channel(destination_channel).send(
+            "A new day has arrived and the ducks feel refreshed from their slumber.")
+    except:
+        print('Error trying to execute the new day.')
 
 
 @client.event
@@ -48,39 +69,44 @@ async def quack(interaction: discord.Interaction):
     with open("./global_info.json", "r") as file:
         global_info = json.load(file)
 
-    user = interaction.user.id
+    user_id = interaction.user.id
+    username = client.get_user(user_id)
     # print(f'{user} trying to quack')
 
     try:
-        if not bool(user_info[str(user)]["quackedToday"]):
-            user_info[user]["quackedToday"] = True
-            user_info[user]["quacks"] += 1
-            user_info[user]["quackStreak"] += 1
-            # print(f'{user} quacked loudly.')
-            message = f'User {user} quacked loudly.'
+        user = user_info[str(user_id)]
 
-            if user_info[user]["quackStreak"] >= global_info["maxQuackStreakLength"]:
-                user_info[user]["quackStreak"] -= global_info["maxQuackStreakLength"]
-                user_info[user]["quacks"] += global_info["quackStreakReward"]
+        if not bool(user["quackedToday"]):
+            user["quackedToday"] = True
+            user["quacks"] += 1
+            user["quackStreak"] += 1
+            # print(f'{user} quacked loudly.')
+            message = f'{username} quacked loudly.'
+
+            if user["quackStreak"] >= global_info["maxQuackStreakLength"]:
+                user["quackStreak"] -= global_info["maxQuackStreakLength"]
+                user["quacks"] += global_info["quackStreakReward"]
                 # print(f'{user} finished a streak and got an extra {global_info["quackStreakReward"]} quacks.')
-                message += f'\nUser {user} finished a streak and got an extra {global_info["quackStreakReward"]} quacks.'
+                message += f'\n{username} finished a streak and got an extra {global_info["quackStreakReward"]} quacks.'
         else:
-            print(
-                f'User {user} tried to quack but your throat is too sore today.')
+            # print(f'User {user} tried to quack but their throat is too sore today.')
+            message = f'{username} tried to quack but their throat is too sore today.'
     except:
         new_user = {
             "quacks": 1,
             "quackStreak": 1,
-            "quackedToday": True
+            "quackedToday": True,
+            "quackRank": ""
         }
-        user_info[user] = new_user
-        print(f'User {user} quacked for the first time!')
+        user_info[user_id] = new_user
+        message = f'{username} quacked for the first time!'
+        # print(f'User {user} quacked for the first time!')
 
     # Save to database
     with open("./user_info.json", "w") as file:
         json.dump(user_info, file, indent=4)
 
-    await interaction.response.send_message("Command executed")
+    await interaction.response.send_message(message)
 
 
 @client.tree.command(name="quackery", description="Check out who are the top quackers.")
@@ -92,7 +118,7 @@ async def quackery(interaction: discord.Interaction, number: int = 10):
 
     for x in range(number):
         user_id = await get_max_quacks(user_info)
-        print(f'user_id: {user_id}')
+        # print(f'user_id: {user_id}')
 
         if user_id == 0:
             break
@@ -122,6 +148,9 @@ async def quack_info(interaction: discord.Interaction, user_id: int = 0):
     with open("./user_info.json", "r") as file:
         user_info = json.load(file)
 
+    with open("./global_info.json", "r") as file:
+        global_info = json.load(file)
+
     if user_id == 0:
         user_id = interaction.user.id
 
@@ -130,12 +159,52 @@ async def quack_info(interaction: discord.Interaction, user_id: int = 0):
         if user_info[str(user_id)]["quackRank"] != "":
             message += f' the {user_info[str(user_id)]["quackRank"]}'
 
-        message += f' has quacked {user_info[str(user_id)]["quacks"]} times and is on a {user_info[str(user_id)]["quacks"]} day streak.'
+        message += f' has quacked {user_info[str(user_id)]["quacks"]} times and is on a {user_info[str(user_id)]["quackStreak"]} day streak. '
+
+        next_rank = await get_next_quack_rank(user_info[str(user_id)]["quackRank"])
+
+        if next_rank != "":
+            quacks = int(user_info[str(user_id)]["quacks"])
+            next_quacks = int(global_info["quackRank"][next_rank])
+
+            message += f'They are {next_quacks-quacks} quacks away from the next rank of {next_rank}.'
 
     except:
         message = 'That user has not quacked yet.'
 
     await interaction.response.send_message(message)
+
+
+async def get_quack_rank(quacks):
+    with open("./global_info.json", "r") as file:
+        global_info = json.load(file)
+
+    quack_rank = ""
+
+    for rank, requirement in global_info["quackRank"].items():
+        if int(quacks) >= int(requirement):
+            quack_rank = rank
+
+    return quack_rank
+
+
+async def get_next_quack_rank(quack_rank):
+    with open("./global_info.json", "r") as file:
+        global_info = json.load(file)
+
+    next_quack_rank = ""
+
+    try:
+        current_quacks = int(global_info["quackRank"][quack_rank])
+    except:
+        current_quacks = 0
+
+    for rank, requirement in global_info["quackRank"].items():
+        # Check greater than current quack rank requirement but lower/eq to target quack rank
+        if requirement > current_quacks and (next_quack_rank == "" or requirement < int(global_info["quackRank"][next_quack_rank])):
+            next_quack_rank = rank
+
+    return next_quack_rank
 
 
 async def main():
