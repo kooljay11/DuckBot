593c9054dc21e0d11145e01cb997d13fcb1e3e52
kooljay11 <86942676+kooljay11@users.noreply.github.com>
2024-02-07 05:32:55 -0800

    Moved reset time to 6am for testing purposes

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ client = commands.Bot(command_prefix="/",
                       intents=discord.Intents.all())
 
 
-@tasks.loop(time=[datetime.time(hour=13, minute=0, tzinfo=datetime.timezone.utc)])
+@tasks.loop(time=[datetime.time(hour=14, minute=0, tzinfo=datetime.timezone.utc)])
 async def dailyReset():
     print('Daily reset occurring')
     with open("./bot_status.txt", "r") as file:
